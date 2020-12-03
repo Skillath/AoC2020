9c3ddff81478a6ae6bb428b7c910b6ba369aaf53
Xabier Gonzalez Goienetxea <xabier.gonzalez.69@gmail.com>
2020-12-03 23:42:24 +0100

    Day 1 Success

---
+++ AoC2020/AoC2020_1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using AoC.Common;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AoC2020
+{
+    // https://adventofcode.com/2020/day/1
+    class AoC2020_1
+    {
+        private const int CurrentDay = 1;
+        private const long Param = 2020L;
+
+        private static IInputLoader _loader;
+
+        static async Task Main(string[] args)
+        {
+            _loader = new InputLoaderFromDisk();
+
+            var input = (await _loader.Load(CurrentDay))
+                .Split('\n')
+                .Select(int.Parse)
+                .ToDictionary(key => (long)key, value => Param - value);
+
+            var output = input.Where(valuePair => input.ContainsKey(valuePair.Value))
+                .Select(i => i.Value)
+                .Aggregate((left, right) => left * right);
+
+
+            Console.WriteLine(output);
+        }
+    }
+}
---
+++ Common/InputDownloader.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AoC.Common
+{
+    public interface IInputLoader
+    {
+        Task<string> Load(int day);
+    }
+
+    public class InputDownloader : IInputLoader
+    {
+        private const string Url = @"https://adventofcode.com/2020/day/{0}/input";
+        private const string FileName = "input_{0}";
+
+
+        public async Task<string> Load(int day)
+        {
+            var url = string.Format(Url, day);
+            var fileName = string.Format(FileName, day);
+
+            using var webClient = new WebClient
+            {
+                Credentials = new CredentialCache()
+                {
+
+                }
+            };
+
+            await webClient.DownloadFileTaskAsync(url, fileName);
+
+
+            return "";
+        }
+    }
+
+    public class InputLoaderFromDisk : IInputLoader
+    {
+        private const string FileName = "input_{0}";
+
+        private readonly string _path;
+
+        public InputLoaderFromDisk(string path = "")
+        {
+            _path = path;
+        }
+
+        public async Task<string> Load(int day)
+        {
+            var fileName = Path.Combine(_path, string.Format(FileName, day));
+
+            using var streamReader = new StreamReader(fileName);
+            return await streamReader.ReadToEndAsync();
+        }
+    }
+}
